9393ad377f3b20c15d33c17dcc99b9764ddb73fd
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-12-21 12:56:16 +0100

    Add bare-bones kitty, fzf, and tmux themes

---
+++ fzf/standard.dark.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+  --color=bg:#0b0c0c,bg+:-1 \
+  --color=border:#2a2c2e \
+  --color=fg:#505a5f,fg+:#f3f2f1 \
+  --color=gutter:#0b0c0c \
+  --color=header:#5694ca \
+  --color=hl:#d53880,hl+:#d53880 \
+  --color=info:#5694ca \
+  --color=marker:#d4351c \
+  --color=pointer:#6f72af \
+  --color=prompt:#505a5f \
+  --color=separator:#2a2c2e \
+  --color=spinner:#b58840"
